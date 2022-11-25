cd83afc8390058c3b30a8b6a99aa2c0b06a8586e
barakadax <63285369+barakadax@users.noreply.github.com>
2022-11-25 10:47:17 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using LinkSolver;
+
+namespace WikiLinkSolver {
+    public class Program {
+        public static async Task Main() {
+            string firstDefinition = "Börek";
+            string searchedDefinition = "Midnight";
+
+            var wikiSolver = new WikiSolver(2);
+
+            await wikiSolver.Run(firstDefinition, searchedDefinition); 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Wikipedia Link Solver<br>
+You create an instance of the object 'WikiSolver' with optinal depth of links to enter until get result in the constructor (default is 3)<br>
+You Run with function you run with 2 parameter, starting page and target page.<br>
+In run also third parameter, choose language of the pages you want to search on, default is 'en'.<br>
